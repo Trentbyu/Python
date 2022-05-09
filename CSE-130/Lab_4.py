27b32f17da6c4d6ca9f1f82714bb3c077dd3a72d
Trentbyu <97129943+Trentbyu@users.noreply.github.com>
2022-05-09 13:10:47 -0600

    push

---
+++ CSE-130/Lab_4.py
@@ -0,0 +1,79 @@
+
+# 1. Name:
+#      -your name-
+# 2. Assignment Name:
+#      Lab 04: Monopoly
+# 3. Assignment Description:
+#      -describe what this program is meant to do-
+# 4. What was the hardest part? Be as specific as possible.
+#      Was it the syntax of Python?
+#      Was it an aspect of the problem you are to solve?
+#      Was it the instructions or any part of the problem definition?
+#      Was it the submission process?
+# 5. How long did it take for you to complete the assignment?
+#      -total time in hours including reading the assignment and submitting the program-
+
+
+
+
+def get_info():
+    
+    pacific_avenue = int(input("what is on  Pacific Avenue? (0:nothing, 1:one house, ... 5:a hotel) "))
+    north_carolina = int(input("what is on  North Carolina? (0:nothing, 1:one house, ... 5:a hotel) "))
+    pennsylvania_avenue = int(input("what is on  Pennsylvania avenu? (0:nothing, 1:one house, ... 5:a hotel) "))
+    cash = int(input("How much cash do you have to spend? "))
+    houses = int(input("How many houses are there to purchase?  "))
+    hotels = int(input("How many hotels are there to purchase? "))
+
+    return pacific_avenue, north_carolina, pennsylvania_avenue, cash, houses, hotels
+
+
+def user_choice():
+    purchase_hotel = int(input("do you want to purchase a hotel type 1? if no type no. "))
+    purchase_house = int(input("do you want to purchase a house type 1-4? if no type no. "))
+    swap_hotel = input("do you want to swap hotel? for no type no ")
+    swap_house = input("do you want to swap house? for no type no ")
+
+    return purchase_hotel, purchase_house, swap_hotel, swap_house
+
+def calculations(pacific_avenue, north_carolina, pennsylvania_avenue, cash, houses, hotels, purchase_hotel, purchase_house, swap_hotel, swap_house):
+
+    price = 200 * purchase_house + 200 * purchase_house
+
+    if price < 200: 
+        print("You do not have sufficient funds to purchase a hotel at this time.")
+    if hotels < 1: 
+        print("There are not enough hotels available for purchase at this time.")
+    if houses < 1: 
+        print("There are not enough houses available for purchase at this time.")
+    if pacific_avenue == 5:
+        print()
+    
+
+    
+    return price 
+    
+def main():
+
+    owned = input("Do you own pacific_avenue, north_carolina, pennsylvania_avenue? Y/N")
+    if owned.lower() == "y":
+
+        pacific_avenue, north_carolina, pennsylvania_avenue, cash, houses, hotels = get_info()
+        purchase_hotel, purchase_house, swap_hotel, swap_house = user_choice()
+        price = calculations(pacific_avenue, north_carolina, pennsylvania_avenue, cash, houses, hotels, purchase_hotel, purchase_house, swap_hotel, swap_house)
+        print(f'''This will cost ${price}.
+        Purchase 1 hotel and [number of houses] house(s).
+        Put 1 hotel on Pennsylvania and return any houses to the bank.
+        Put [number of houses] house(s) on North Carolina.
+        Put [number of houses] house(s) on Pacific.''')
+    else: 
+        print("You cannot purchase a hotel until you own all the properties of a given color group.")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
--- CSE-130/read.py
+++ CSE-130/read.py
@@ -1,8 +1,44 @@
 
 
-name = "trent"
-Last = "black"
-age = "age "
 
-print(f'{"name":-<25} {"Last":_<25} {"age":_<25} ')
-print(f"{name:-<25} {Last:_<25} {age:_<25} ")
+
+import random
+from string import digits
+from string import punctuation
+from string import ascii_letters
+
+
+password = ' '
+
+symbol = digits 
+secrue_random = random.SystemRandom()
+
+
+for i in range(1):
+    password += secrue_random.choice(symbol)
+
+print(password)
+
+guess = ''
+guesses = []
+
+while password != guess:
+
+    if len(guess) <= 1 :
+        guess += secrue_random.choice(symbol)
+        if guess in guesses[0::1]:
+           stored = guess[1]
+           guess = stored
+    else: 
+        guesses.append(guess)
+        guess = " "
+
+            
+
+print(guess[0])
+print(len(guesses))
+print(guesses[0::1])
+print(guess[1])
+
+
+
